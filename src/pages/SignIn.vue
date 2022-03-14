664631e64960ff082f4dab2c778dfb1ede6d7d37
Rafael Araujo <rflaraujodev@gmail.com>
2022-03-14 16:15:38 -0300

    init routes

---
+++ src/pages/SignIn.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card flat bordered>
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+      <q-card-section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    component: () => import('@/layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '/',
+        name: 'Main',
+        component: () => import('@/pages/Main.vue')
+      },
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '',
+    component: () => import('@/layouts/DefaultLayout.vue'),
+    children: [
+      {
+        path: '/signin',
+        name: 'Auth',
+        component: () => import('@/pages/SignIn.vue')
+      }
+    ]
+  },
 ]
 
 const router = new VueRouter({
